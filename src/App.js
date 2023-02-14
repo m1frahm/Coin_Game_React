cb0ef091445d5d00362119e995a54b5203bf41b6
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-14 17:14:44 -0500

    1 - Used state hook to create [coin, setCoin]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import CoinImage from "./components/CoinImage";
 
 // function App() {
 //   return (
@@ -25,16 +27,22 @@ import './App.css';
 // export default App;
 
 export default function App() {
+  const [coin, setCoin] = useState(null);
+  // coin is the value
+// setCoin is the function that changes the value of coin
+
   function flipCoin() {
     // assign random value where 0 = heads & 1 = tails
     var flip = Math.floor(Math.random() * 2);
     console.log(flip);
+    setCoin(flip) // now the value of coin will be 0 or 1 bc we passed  
   }
 
   return (
     <div className="App">
     <header>COIN GAME</header>
-      <button>Click Me To Flip</button>
+    <CoinImage coin={coin}></CoinImage>
+      <button onClick={flipCoin}>Click Me To Flip</button>
       <button>Reset</button>
       <div> Score Count PlaceHolder </div>
       <div> Head Count </div>
--- src/components/CoinImage.js
+++ src/components/CoinImage.js
@@ -1,10 +1,17 @@
-const headImage =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY8WGH5R7vPUEYqU75g659_cq7qWdSZ7zC7nUo8gK0hQoPV-gEK2Ho1AxwzXpax5ggtl0&usqp=CAU";
-const tailImage =
-  "https://us.123rf.com/450wm/spideyspike/spideyspike2007/spideyspike200700006/spideyspike200700006.jpg?ver=6";
 
 function CoinImage(props) {
-  return <div></div>;
-}
+  const headImage =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY8WGH5R7vPUEYqU75g659_cq7qWdSZ7zC7nUo8gK0hQoPV-gEK2Ho1AxwzXpax5ggtl0&usqp=CAU";
+  const tailImage =
+  "https://us.123rf.com/450wm/spideyspike/spideyspike2007/spideyspike200700006/spideyspike200700006.jpg?ver=6";
+  if (props.coin === 0) {
+    return <img src={headImage}/> // variables must always be in {} 
+      } else {
+        return <img src={tailImage}/>
+      }
+    }
+
+    // add to line 9
 
 export default CoinImage;
+
