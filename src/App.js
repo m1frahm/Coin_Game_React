15366cdf4f52a6e144ff50477458afdc38f3a44e
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-14 12:54:43 -0500

    base complete, now need to add functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  function flipCoin() {
+    // assign random value where 0 = heads & 1 = tails
+    var flip = Math.floor(Math.random() * 2);
+    console.log(flip);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>COIN GAME</header>
+      <button>Click Me To Flip</button>
+      <button>Reset</button>
+      <div> Score Count PlaceHolder </div>
+      <div> Head Count </div>
+      <div> Tail Count </div>
+      <div> Win Message Placeholder </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/CoinImage.js
@@ -0,0 +1,10 @@
+const headImage =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY8WGH5R7vPUEYqU75g659_cq7qWdSZ7zC7nUo8gK0hQoPV-gEK2Ho1AxwzXpax5ggtl0&usqp=CAU";
+const tailImage =
+  "https://us.123rf.com/450wm/spideyspike/spideyspike2007/spideyspike200700006/spideyspike200700006.jpg?ver=6";
+
+function CoinImage(props) {
+  return <div></div>;
+}
+
+export default CoinImage;
