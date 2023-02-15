479bb2200860d98e62bd646709812593af858763
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-14 20:52:49 -0500

    used props.heads and props.tails in Winmessage child component

--- src/components/WinMessage.js
+++ src/components/WinMessage.js
@@ -3,9 +3,9 @@ function WinMessage(props) {
     const headWins = "The Winner is HEADS!";
     const tailWins = "The Winner is TAILS!";
     const forNull = "You have not flipped your coin yet!"
-    if (props.coin === 0) {
+    if (props.coin === 0 && props.heads === 5) {
       return <text>{headWins}</text> // variables must always be in {} 
-        } else if (props.coin === 1) {
+        } else if (props.coin === 1 && props.heads === 5) {
           return <text>{tailWins}</text> 
           } else if (props.coin === null) {
           return <text>{forNull}</text>
